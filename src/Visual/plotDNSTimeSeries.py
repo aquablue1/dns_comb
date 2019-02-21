697588d09fddb29198bdcc5592bf964fdfb2a16b
weisser <94apieceofcake@gmail.com>
2019-02-20 21:48:53 -0700

    Measure the aurora server traffic.

--- src/Visual/plotDNSTimeSeries.py
+++ src/Visual/plotDNSTimeSeries.py
@@ -7,46 +7,104 @@ import matplotlib.pyplot as plt
 import json
 
 
+def getTTLV(curTTLV):
+    if curTTLV <= -25:
+        return 25
+    else:
+        return curTTLV-5
+
+
 def getData(filename):
     targetIPC = "207.204.213.x"
     targetIPC = "64.x.x.x"
     # targetIPC = "218.248.112.x"
+    targetIPC = "190.157.x.x"
+    targetIPC = "198.134.2.x"
     with open(filename, 'r') as f:
         rawData = json.load(f)[targetIPC]
-    timeStart = 1535760000 + 3600*24*3 + 6*3600
-    timeEnd = 1535760000 + 3600*24*4 + 6*3600
+    # 1535781600 or 1535760000
+    timeStart = 1535781600 + 3600*24*3
+    timeEnd = 1535781600 + 3600*24*6
 
     # timeStart = 1536573960
     # timeEnd = 1536574080
-    dstIP1 = "136.159.51.4"
-    ip1List = []
-    dstIP2 = "136.159.51.5"
-    ip2List = []
-    dstIP3 = "136.159.52.10"
-    ip3List = []
+    ttlLevel = 30
+    replyList = []
+    emptyList = []
 
     for recocrd in rawData:
         print(recocrd)
         ts = float(recocrd[0])
-        if timeStart <= ts <= timeEnd:
-            info = [recocrd[0], recocrd[3], recocrd[4]]
-            if recocrd[2] == dstIP1:
-                ip1List.append(info)
-            elif recocrd[2] == dstIP2:
-                ip2List.append(info)
-            elif recocrd[2] == dstIP3:
-                ip3List.append(info)
+        if timeStart <= ts <= timeEnd and recocrd[2] == "136.159.142.4"\
+                and recocrd[5] == "ns1.auroralimaging.com":
+            reply = [recocrd[0], recocrd[3], recocrd[4]]
+            if recocrd[6]!="-":
+                replyList.append(reply)
+            else:
+                emptyList.append(reply)
+
     # print(ip1List)
-    for elem in ip1List:
+    replyClr = (0.6, 0.0, 0.0, 0.2)
+    for elem in replyList:
         if elem[0] == "-" or elem[1] == "-" or elem[2] == "-":
             continue
         print(elem)
         plt.plot([float(elem[0]), float(elem[0])], [float(elem[1]), 0-float(elem[2])],
-                 color="black")
+                 color=replyClr, linewidth=2)
+        ttlLevel = getTTLV(ttlLevel)
+        plt.plot([float(elem[0]), float(elem[0])+3600], [ttlLevel, ttlLevel])
+
+    emptyClr = (0.0, 0.6, 0.0, 0.2)
+    for elem in emptyList:
+        if elem[0] == "-" or elem[1] == "-" or elem[2] == "-":
+            continue
+        print(elem)
+        plt.plot([float(elem[0]), float(elem[0])], [float(elem[1]), 0-float(elem[2])],
+                 color=emptyClr, linewidth=2)
+        # plt.plot([float(elem[0]), float(elem[0])+3600], [25, 25])
+
+    print(len(replyList))
+    print(len(emptyList))
+
+    replyList = []
+    emptyList = []
+
+    for recocrd in rawData:
+        print(recocrd)
+        ts = float(recocrd[0])
+        if timeStart <= ts <= timeEnd and recocrd[2] == "136.159.142.5"\
+                and recocrd[5] == "ns1.auroralimaging.com":
+            reply = [recocrd[0], recocrd[3], recocrd[4]]
+            if recocrd[6]!="-":
+                replyList.append(reply)
+            else:
+                emptyList.append(reply)
+
+    # print(ip1List)
+    replyClr = (0.6, 0.0, 0.0, 0.2)
+    for elem in replyList:
+        if elem[0] == "-" or elem[1] == "-" or elem[2] == "-":
+            continue
+        print(elem)
+        ttlLevel = getTTLV(ttlLevel)
+        plt.plot([float(elem[0]), float(elem[0])], [float(elem[1]), 0-float(elem[2])],
+                 color=replyClr, linewidth=2, linestyle="--")
+        plt.plot([float(elem[0]), float(elem[0])+3600], [ttlLevel, ttlLevel])
+
+    emptyClr = (0.0, 0.6, 0.0, 0.2)
+    for elem in emptyList:
+        if elem[0] == "-" or elem[1] == "-" or elem[2] == "-":
+            continue
+        print(elem)
+        plt.plot([float(elem[0]), float(elem[0])], [float(elem[1]), 0-float(elem[2])],
+                 color=emptyClr, linewidth=2, linestyle="--")
+    print(len(replyList))
+    print(len(emptyList))
     plt.plot([timeStart-3600, timeEnd+3600], [0,0], linestyle="--", color="black", linewidth=1)
     plt.xlim((timeStart, timeEnd))
+
     plt.xticks(list(range(timeStart, timeEnd+3600*24, 3600*24)),
-               ["2018-09-%s" % str(d).zfill(2) for d in range(4, 11)], rotation=15)
+               ["2018-09-%s" % str(d).zfill(2) for d in range(4, 7)], rotation=15)
     # plt.xticks([1536573960, 1536573960+60, 1536573960+120],
     #            ["2018-09-10 10:06", "2018-09-10 10:07", "2018-09-10 10:08"], rotation=15)
     plt.show()
@@ -59,7 +117,7 @@ if __name__ == '__main__':
 
     targetList += ["outakamai", "outcampus1", "outcampus2",
                   "outcpsc", "outothers", "outwebpax"]
-    targetList = ["inphys"]
+    targetList = ["inaurora"]
     for target in targetList:
-        filename = "../../exchange/batchedTransWork/%sTransPop100T.log" % target
+        filename = "../../exchange/batchedTransWork/%sTransTop100TSpecial.log" % target
         getData(filename)
