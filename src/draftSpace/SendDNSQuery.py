be2a79ae3ee80b10fc9c6d0b9faeddb321bbf2a4
weisser <94apieceofcake@gmail.com>
2019-02-26 12:32:23 -0700

    ready dns query test

--- src/draftSpace/SendDNSQuery.py
+++ src/draftSpace/SendDNSQuery.py
@@ -1,9 +1,13 @@
+import sys
+import time
 import dns.resolver
 
 
-domain = 'google.com'
-domain = 'www.cpsc.ucalgary.ca'
-answers = dns.resolver.query(domain,'A')
-print(answers)
-for server in answers:
-    print(server)
+# domain = 'google.com'
+domain = 'auroralimaging.ca'
+for _ in range(0, 100):
+    answers = dns.resolver.query(domain,'NS')
+    # print(answers)
+    for server in answers:
+        print(server)
+    time.sleep(1)
