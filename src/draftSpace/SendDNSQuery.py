a644c17d22eb968a6390b4109d87ec1e17cb5ac6
weisser <94apieceofcake@gmail.com>
2019-02-27 20:06:55 -0700

    backup push

--- src/draftSpace/SendDNSQuery.py
+++ src/draftSpace/SendDNSQuery.py
@@ -3,11 +3,21 @@ import time
 import dns.resolver
 
 
-# domain = 'google.com'
-domain = 'ns1.auroralimaging.ca'
-for _ in range(0, 60):
-    answers = dns.resolver.query(domain,'A')
-    # print(answers)
-    for server in answers:
-        print(server)
-    time.sleep(1)
+# domain = 'www.google.com'
+domain = 'auroralimaging.com'
+# domain = 'cpsc.ucalgary.ca'
+domain = "madrigal.phys.ucalgary.ca"
+my_resolver = dns.resolver.Resolver(configure=False)
+my_resolver.nameservers = ["136.159.142.5"]
+for _ in range(0, 1):
+    try:
+        answers = my_resolver.query(domain,'A')
+        # print(answers)
+        # for server in answers:
+            # print(server.to_text(origin=True))
+        print(answers.response)
+    except:
+        print("Error Info")
+    finally:
+        time.sleep(1)
+        # dns.resolver.reset_default_resolver()
